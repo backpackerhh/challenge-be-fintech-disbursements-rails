9d19a68afcff62329538e3ad688ec2e789493caf
David Montesdeoca <backpackerhh@gmail.com>
2024-03-21 17:40:45 +0100

    Add unit test for jobs to ensure their configuration is correct

---
+++ spec/contexts/payments_context/merchants/jobs/create_merchant_job_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe PaymentsContext::Merchants::Jobs::CreateMerchantJob, type: :job do
+  it "is executed in expected queue" do
+    expect(described_class.queue_name).to eq("import_data")
+  end
+
+  it "has expected configuration" do
+    expect(described_class.sidekiq_options.transform_keys(&:to_sym)).to match(
+      hash_including(
+        unique: true,
+        retry: true,
+        retry_for: 3600
+      )
+    )
+  end
+end
